60a3b559747415f33a006d00893ce53431fcf2e3
Oh Sangho <dhtkdgh429@gmail.com>
2023-06-24 13:06:40 +0900

    initial commit

---
+++ Mungroad-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mungroad-iOS
+//
+//  Created by Oh Sangho on 2023/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
